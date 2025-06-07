9330ea84583d11494c1f7b6c6a7178a4c1906b6b
Y Ethan Guo <ethan.guoyihua@gmail.com>
2025-06-07 00:12:53 -0700

    [HUDI-9506] Lazily load meta client and file system view to improve Hudi connector performance

--- plugin/trino-hudi/src/main/java/io/trino/plugin/hudi/HudiTableHandle.java
+++ plugin/trino-hudi/src/main/java/io/trino/plugin/hudi/HudiTableHandle.java
@@ -17,16 +17,22 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableSet;
+import io.trino.metastore.Table;
 import io.trino.plugin.hive.HiveColumnHandle;
+import io.trino.spi.TrinoException;
 import io.trino.spi.connector.ConnectorTableHandle;
 import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.predicate.TupleDomain;
 import org.apache.hudi.common.model.HoodieTableType;
+import org.apache.hudi.common.table.HoodieTableMetaClient;
+import org.apache.hudi.common.table.timeline.HoodieInstant;
+import org.apache.hudi.util.Lazy;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static io.trino.spi.connector.SchemaTableName.schemaTableName;
 import static java.util.Objects.requireNonNull;
 
@@ -37,12 +43,14 @@ public class HudiTableHandle
     private final String tableName;
     private final String basePath;
     private final HoodieTableType tableType;
-    private final Optional<String> preCombineField;
     private final List<HiveColumnHandle> partitionColumns;
     // Used only for validation when config property hudi.query-partition-filter-required is enabled
     private final Set<HiveColumnHandle> constraintColumns;
     private final TupleDomain<HiveColumnHandle> partitionPredicates;
     private final TupleDomain<HiveColumnHandle> regularPredicates;
+    // Coordinator-only
+    private final Optional<Table> table;
+    private final Optional<Lazy<HoodieTableMetaClient>> lazyMetaClient;
 
     @JsonCreator
     public HudiTableHandle(
@@ -50,34 +58,61 @@ public class HudiTableHandle
             @JsonProperty("tableName") String tableName,
             @JsonProperty("basePath") String basePath,
             @JsonProperty("tableType") HoodieTableType tableType,
-            @JsonProperty("preCombineField") String preCombineField,
             @JsonProperty("partitionColumns") List<HiveColumnHandle> partitionColumns,
             @JsonProperty("partitionPredicates") TupleDomain<HiveColumnHandle> partitionPredicates,
             @JsonProperty("regularPredicates") TupleDomain<HiveColumnHandle> regularPredicates)
     {
-        this(schemaName, tableName, basePath, tableType, Optional.ofNullable(preCombineField), partitionColumns, ImmutableSet.of(), partitionPredicates, regularPredicates);
+        this(Optional.empty(), Optional.empty(), schemaName, tableName, basePath, tableType, partitionColumns, ImmutableSet.of(), partitionPredicates, regularPredicates);
     }
 
     public HudiTableHandle(
+            Optional<Table> table,
+            Optional<Lazy<HoodieTableMetaClient>> lazyMetaClient,
             String schemaName,
             String tableName,
             String basePath,
             HoodieTableType tableType,
-            Optional<String> preCombineField,
             List<HiveColumnHandle> partitionColumns,
             Set<HiveColumnHandle> constraintColumns,
             TupleDomain<HiveColumnHandle> partitionPredicates,
             TupleDomain<HiveColumnHandle> regularPredicates)
     {
+        this.table = requireNonNull(table, "table is null");
+        this.lazyMetaClient = requireNonNull(lazyMetaClient, "lazyMetaClient is null");
         this.schemaName = requireNonNull(schemaName, "schemaName is null");
         this.tableName = requireNonNull(tableName, "tableName is null");
         this.basePath = requireNonNull(basePath, "basePath is null");
         this.tableType = requireNonNull(tableType, "tableType is null");
-        this.preCombineField = requireNonNull(preCombineField, "preCombineField is null");
         this.partitionColumns = requireNonNull(partitionColumns, "partitionColumns is null");
         this.constraintColumns = requireNonNull(constraintColumns, "constraintColumns is null");
         this.partitionPredicates = requireNonNull(partitionPredicates, "partitionPredicates is null");
         this.regularPredicates = requireNonNull(regularPredicates, "regularPredicates is null");
+    }
+
+    public Table getTable()
+    {
+        checkArgument(table.isPresent(),
+                "getTable() called on a table handle that has no metastore table object; "
+                        + "this is likely because it is called on the worker.");
+        return table.get();
+    }
+
+    public HoodieTableMetaClient getMetaClient()
+    {
+        checkArgument(lazyMetaClient.isPresent(),
+                "getMetaClient() called on a table handle that has no Hudi meta-client; "
+                        + "this is likely because it is called on the worker.");
+        return lazyMetaClient.get().get();
+    }
+
+    public String latestCommitTime()
+    {
+        return getMetaClient().getActiveTimeline()
+                .getCommitsTimeline()
+                .filterCompletedInstants()
+                .lastInstant()
+                .map(HoodieInstant::requestedTime)
+                .orElseThrow(() -> new TrinoException(HudiErrorCode.HUDI_NO_VALID_COMMIT, "Table has no valid commits"));
     }
 
     @JsonProperty
@@ -102,12 +137,6 @@ public class HudiTableHandle
     public HoodieTableType getTableType()
     {
         return tableType;
-    }
-
-    @JsonProperty
-    public Optional<String> getPreCombineField()
-    {
-        return preCombineField;
     }
 
     @JsonProperty
@@ -146,11 +175,12 @@ public class HudiTableHandle
             TupleDomain<HiveColumnHandle> regularTupleDomain)
     {
         return new HudiTableHandle(
+                table,
+                lazyMetaClient,
                 schemaName,
                 tableName,
                 basePath,
                 tableType,
-                preCombineField,
                 partitionColumns,
                 constraintColumns,
                 partitionPredicates.intersect(partitionTupleDomain),
